6e0f9031d27d563a6c6205ec204f9b1cc7272c7f
HansMartinJ <hansmartinjohansen@protonmail.com>
2020-01-31 16:19:10 +0100

    Added functions to IMapPiece

--- src/main/java/Interface/IMapPiece.java
+++ src/main/java/Interface/IMapPiece.java
@@ -1,4 +1,15 @@
 package Interface;
 
 public interface IMapPiece {
+
+    void setPos(IPosition pos);
+    IPosition getPos();
+
+    /**
+     * Do an end of the turn action based on the type of the map piece
+     */
+    void doTurn();
+
+
+
 }
