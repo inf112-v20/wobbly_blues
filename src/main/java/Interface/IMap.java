84d0685201788bf9a7d2a13f47effbe8823a2677
Tines Valen <Tines.Valen@student.uib.no>
2020-01-31 13:35:45 +0100

    added methods

--- src/main/java/Interface/IMap.java
+++ src/main/java/Interface/IMap.java
@@ -3,6 +3,9 @@ package Interface;
 import Enums.Direction;
 
 public interface IMap {
+    void setCell();
     boolean canGo(Direction dir);
     ICell getCell(int x, int y);
+    void createRobot();
+
 }
