6c2b859fa3218f2b38a0e75c63ebf03336286f28
Sindre Kristensen <quv011@uib.no>
2020-02-04 12:33:39 +0100

    Is showing a player on the map

--- src/main/java/inf112/skeleton/app/HelloWorld.java
+++ src/main/java/inf112/skeleton/app/HelloWorld.java
@@ -1,40 +1,48 @@
 package inf112.skeleton.app;
 
-import com.badlogic.gdx.ApplicationListener;
-import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.*;
 import com.badlogic.gdx.graphics.*;
 import com.badlogic.gdx.graphics.g2d.*;
 import com.badlogic.gdx.maps.tiled.*;
 import com.badlogic.gdx.maps.tiled.renderers.*;
+import com.badlogic.gdx.maps.tiled.tiles.*;
+import com.badlogic.gdx.math.*;
 
 
-public class HelloWorld implements ApplicationListener {
-    private SpriteBatch batch;
-    private BitmapFont font;
+public class HelloWorld extends InputAdapter implements ApplicationListener {
     private TiledMap map;
     private TiledMapTileLayer Board;
-    private TiledMapTileLayer Flag;
-    private TiledMapTileLayer Hole;
-    private TiledMapTileLayer Player;
+    private TiledMapTileLayer FlagLayer;
+    private TiledMapTileLayer HoleLayer;
+    private TiledMapTileLayer PlayerLayer;
     private OrthogonalTiledMapRenderer TMrenderer;
     private OrthographicCamera Camera;
+    private Texture Robot;
+    private TiledMapTileLayer.Cell Normal;
+    private TiledMapTileLayer.Cell Dead;
+    private TiledMapTileLayer.Cell Won;
+    private Vector2 PlayerPos;
 
     @Override
     public void create() {
-        batch = new SpriteBatch();
-        font = new BitmapFont();
-        font.setColor(Color.RED);
+        Gdx.input.setInputProcessor(this);
         map = new TmxMapLoader().load("example.xml");
         Board = (TiledMapTileLayer) map.getLayers().get("Board");
-        Flag = (TiledMapTileLayer) map.getLayers().get("Flag");
-        Hole = (TiledMapTileLayer) map.getLayers().get("Hole");
-        Player = (TiledMapTileLayer) map.getLayers().get("Player");
+        FlagLayer = (TiledMapTileLayer) map.getLayers().get("Flag");
+        HoleLayer = (TiledMapTileLayer) map.getLayers().get("Hole");
+        PlayerLayer = (TiledMapTileLayer) map.getLayers().get("Player");
+        Robot = new Texture(Gdx.files.internal("player.png"));
+        PlayerPos = new Vector2();
         Camera = new OrthographicCamera();
         TMrenderer = new OrthogonalTiledMapRenderer(map, (float) 0.00333);
+
+        Normal = new TiledMapTileLayer.Cell().setTile(new StaticTiledMapTile(new TextureRegion(Robot,300,300)));
+        PlayerPos.set(0,0);
 
         Camera.setToOrtho(false,5,5);
         float h = Camera.viewportHeight;
         float w = Camera.viewportWidth;
+
         Camera.position.set(h/2,w/2,0);
         Camera.update();
 
@@ -43,19 +51,14 @@ public class HelloWorld implements ApplicationListener {
 
     @Override
     public void dispose() {
-        batch.dispose();
-        font.dispose();
     }
 
     @Override
     public void render() {
         Gdx.gl.glClearColor(1, 1, 1, 1);
         Gdx.gl.glClear(GL30.GL_COLOR_BUFFER_BIT);
-
+        PlayerLayer.setCell(0,0,Normal);
         TMrenderer.render();
-        batch.begin();
-        font.draw(batch, "Hello World", 200, 200);
-        batch.end();
     }
 
     @Override
@@ -68,5 +71,10 @@ public class HelloWorld implements ApplicationListener {
 
     @Override
     public void resume() {
+    }
+
+    @Override
+    public boolean keyUp(int keycode) {
+        return super.keyUp(keycode);
     }
 }
