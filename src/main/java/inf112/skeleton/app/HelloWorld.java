558ccb41698dcf599c933353eebd121fdc9d673a
Sindre Kristensen <quv011@uib.no>
2020-02-04 09:49:21 +0100

    set a background and made a robot move. added the images

--- src/main/java/inf112/skeleton/app/HelloWorld.java
+++ src/main/java/inf112/skeleton/app/HelloWorld.java
@@ -1,7 +1,6 @@
 package inf112.skeleton.app;
 
-import com.badlogic.gdx.ApplicationListener;
-import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.*;
 import com.badlogic.gdx.graphics.*;
 import com.badlogic.gdx.graphics.g2d.*;
 
@@ -9,13 +8,22 @@ public class HelloWorld implements ApplicationListener {
     private SpriteBatch batch;
     private BitmapFont font;
     private Sprite background;
+    private Sprite player;
+    private float w;
+    private float h;
 
     @Override
     public void create() {
+        w = Gdx.graphics.getWidth();
+        h = Gdx.graphics.getHeight();
+
         batch = new SpriteBatch();
         font = new BitmapFont();
         background = new Sprite( new Texture(Gdx.files.internal("Chasm.bmp")));
+        player = new Sprite(( new Texture((Gdx.files.internal(("testRobot.bmp"))))));
         background.setSize(900,900);
+        player.setSize(70,70);
+        player.setPosition(0,0);
         font.setColor(Color.RED);
     }
 
@@ -29,10 +37,34 @@ public class HelloWorld implements ApplicationListener {
     public void render() {
         Gdx.gl.glClearColor(1, 1, 1, 1);
         Gdx.gl.glClear(GL30.GL_COLOR_BUFFER_BIT);
+        if(Gdx.input.isKeyPressed(Input.Keys.LEFT )&& player.getX() > 0){
+            if(Gdx.input.isKeyPressed(Input.Keys.CONTROL_LEFT))
+                player.translateX(-1f);
+            else
+                player.translateX(-10.0f);
+        }
+        if(Gdx.input.isKeyPressed(Input.Keys.RIGHT) && player.getX() < 830){
+            if(Gdx.input.isKeyPressed(Input.Keys.CONTROL_LEFT))
+                player.translateX(1f);
+            else
+                player.translateX(10.0f);
+        }
+        if(Gdx.input.isKeyPressed(Input.Keys.UP )&& player.getY() < 830){
+            if(Gdx.input.isKeyPressed(Input.Keys.CONTROL_LEFT))
+                player.translateY(1f);
+            else
+                player.translateY(10.0f);
+        }
+        if(Gdx.input.isKeyPressed(Input.Keys.DOWN) && player.getY() > 0){
+            if(Gdx.input.isKeyPressed(Input.Keys.CONTROL_LEFT))
+                player.translateY(-1f);
+            else
+                player.translateY(-10.0f);
+        }
 
         batch.begin();
         background.draw(batch);
-        font.draw(batch, "Hello World", 430, 450);
+        player.draw(batch);
         batch.end();
     }
 
