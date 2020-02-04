423ce3d7674f452394bd6718db57ab71c9563171
Sindre Kristensen <quv011@uib.no>
2020-02-04 08:40:19 +0100

    Finished the robot interface

--- src/main/java/Interface/IRobot.java
+++ src/main/java/Interface/IRobot.java
@@ -1,4 +1,30 @@
 package Interface;
 
 public interface IRobot {
+
+    /*
+    this does not need to take in anything or return, because it is an internal action
+     */
+    void takeDamage();
+    /*
+    It shoots the laser
+     */
+    void shootLaser();
+    /*
+    determins what the robot is going to do
+     */
+    void input();
+    /*
+    tels the system if the robot is going to power down or not
+     */
+    boolean powerDown();
+    /*
+    Creats the hand that holds the program cards that relates to that robot
+     */
+    void creatHand();
+
+    /*
+    not sure
+     */
+    void getDirection();
 }
--- src/main/java/inf112/skeleton/app/HelloWorld.java
+++ src/main/java/inf112/skeleton/app/HelloWorld.java
@@ -1,11 +1,12 @@
 package inf112.skeleton.app;
-
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL30;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+
 
 public class HelloWorld implements ApplicationListener {
     private SpriteBatch batch;
