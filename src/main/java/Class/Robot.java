7fade1cff38192479cb853b69da58ee533ad225b
Sindre Kristensen <quv011@uib.no>
2020-02-07 11:08:21 +0100

    changes

--- src/main/java/Class/Robot.java
+++ src/main/java/Class/Robot.java
@@ -2,13 +2,11 @@ package Class;
 
 import Enums.Direction;
 import Interface.*;
-import com.badlogic.gdx.graphics.*;
 import com.badlogic.gdx.maps.tiled.*;
 import com.badlogic.gdx.math.*;
 
 public class Robot implements IRobot {
 
-    private Texture Robot;
     private TiledMapTileLayer.Cell state;
 
     private Vector2 Pos;
