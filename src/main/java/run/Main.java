ee00f75b53c95102fe3a9760e7aa89830b0d071a
Sindre Kristensen <quv011@uib.no>
2020-02-28 10:34:26 +0100

    nothing

--- src/main/java/run/Main.java
+++ src/main/java/run/Main.java
@@ -3,14 +3,13 @@ package run;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 
-
 public class Main {
     public static void main(String[] args) {
-
         LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
+
         cfg.title = "Robo-Rally";
-        cfg.width = 1200;
-        cfg.height = 1200;
+        cfg.width = 500;
+        cfg.height = 500;
 
         new LwjglApplication(new Board(), cfg);
     }
