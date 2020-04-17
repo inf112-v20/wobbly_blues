6a422e2efa8f032cfcf34d1cb68b8bcd5fdd6293
Sindre Kristensen <quv011@uib.no>
2020-04-17 13:05:47 +0200

    videre utvikling av Laser funksjon, ikke fungerende helt end

---
+++ src/main/java/classes/Laser.java
@@ -0,0 +1,23 @@
+package classes;
+
+import com.badlogic.gdx.math.*;
+import enums.*;
+
+public class Laser {
+
+    private Vector2 pos;
+    private Direction dir;
+
+    public Laser(Vector2 pos, Direction dir){
+        this.pos = pos;
+        this.dir = dir;
+    }
+
+    public Vector2 getPos() {
+        return pos;
+    }
+
+    public Direction getDir() {
+        return dir;
+    }
+}
